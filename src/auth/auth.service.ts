0f3c239877a9b4b88b9613f2582eec520c541775
gobinda1547 <gobinda.paul.1547@gmail.com>
2024-12-13 06:39:23 -0600

    Write 2 sample endpoints

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    // will write signup related business logic here
+    signup() {
+        return 'I am sign up'
+    }
+
+    // will write signin related business logic here
+    signin() {
+        return 'I am sign in'
+    }
+}
